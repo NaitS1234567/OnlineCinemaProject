be85b9563da74ced9bde2912dfc8dfb98ac83639
NaitS <aldardabn@gmail.com>
2022-12-15 21:06:41 +1000

    Authorization_check_user_2

--- films.c
+++ films.c
@@ -10,16 +10,12 @@ struct film {
     char* genre;
     float reit;
 };
-
 struct all_films{
     struct film;
 };
 struct film list_films(FILE* infile){
 
 }
-
-
-
 struct user {
     char name;
     char password;
@@ -33,8 +29,36 @@ void ERROR(char * s){
     printf("ERROR - check %s", s);
 }
 
-void
-sign_up(FILE* users){
+int check_exist_user(FILE* file, char * user_name) {
+    int len_exist = strlen(user_name);
+    printf("%s\n", user_name);
+    char *s[30];
+    int k = 0;
+    while (!feof(file)) {
+        if (k%5 == 0){
+            int flag = 0;
+            fscanf(file,"%s",&s);
+            int l = strlen(s);
+            if (l == len_exist){
+                for (int i = 0; i < l; i++){
+                    if (s[i] != user_name[i]){
+                        flag = 1;
+                        break;
+                    }
+                }
+            if (flag == 1){
+                return 1;
+            }
+            }
+        } else {
+            fscanf(file,"%s", &s);
+        }
+        k++;
+    }
+    return 0;
+}
+
+void sign_up(FILE* users){
     char ** mas = (char**)malloc(sizeof(char*)*3);
     for (int i = 0; i < 3; i++){
         mas[i] = (char*)malloc(sizeof(char)*20);
@@ -44,19 +68,20 @@ sign_up(FILE* users){
     int is_admin = 0;
     int len;
 
-    while (1) {
+    while (1) { // user name scanf
         char s[20];
         printf("Print your name (len 3-20) ");
         scanf("%s", &s);
         len = strlen(s);
-        if (len < 3 || len > 20) {
+        int fix = check_exist_user(users, s);
+        if (len < 3 || len > 20 || fix == 1) {
             ERROR("name\n");
             continue;
         }
         mas[0] = s;
         break;
     }
-    while (1){
+    while (1){ // password scanf
         char s1[20];
         printf("Create your password (len 6-20) ");
         scanf("%s", &s1);
@@ -69,20 +94,30 @@ sign_up(FILE* users){
         break;
     }
 
-    while (1){
+    while (1){ // card nums scanf
         char s2[20];
-        printf("MONEY PISHI EPT (len 16) ");
+        printf("MONEY PISHI EPT (len 16) "); // MAKE check string in card nums !!!!!!!!!!!!!!!!!!!
         scanf("%s", &s2);
         len = strlen(s2);
         if (len != 5){
             ERROR("card nums\n");
             continue;
         }
+        int card_flag = 0;
+        for (int i = 0; i < len; i++){ // CHECK STRING IN CARD NUMS
+            if ( '0' > s2[i] || s2[i] > '9'){
+                ERROR("card nums\n");
+                card_flag = 1;
+                break;
+            }
+        }
+        if (card_flag == 1)
+            continue;
         mas[2] = s2;
         break;
     }
 
-
+    fprintf(users,"%s","&&\n");
     for (int i = 0; i < 3; i++){
         printf("%s \n", mas[i]);
         fprintf(users, "%s", mas[i]);
@@ -91,10 +126,9 @@ sign_up(FILE* users){
     fprintf(users, "%d", len_favourites);
     fprintf(users,"%c",'\n');
     fprintf(users, "%d", is_admin);
-    fprintf(users,"%s","\n&&\n");
+    fprintf(users, "%c", '\n');
     printf("Registration was successful!\n");
 }
-
 
 void authorization(FILE* users){
     printf("=======================\n");
@@ -108,29 +142,20 @@ void authorization(FILE* users){
             printf("ERROR - write again \n");
             printf("        |print ");
         } else if (key == 2) {
-            printf("\n====== Registration ======\n");
+            printf("\n========== Registration ==========\n");
             sign_up(users);
             break;
         } else {
-            printf("\n====== Login ======\n");
+            printf("\n========== Login ==========\n");
             break;
         }
     }
 
-
 }
-
-
-
-
-
 
 int main(){
     FILE* users = fopen("users.txt","a+");
     authorization(users);
-
-
-
 
 
     return 0;
