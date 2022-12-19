3bad2d57873023ecbeb30a6146cbded820753a54
ACwarq <ievlevios65@gmail.com>
2022-12-19 14:19:30 +1000

    Made some basic UX\UI, Changed text messages, added a couple system("cls") events, cleared some extra code. Planning to do more ASAP. Stay tuned!

---
+++ cmake-build-debug/CMakeFiles/OnlineCinemaProjectMy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OnlineCinemaProjectMy.dir/films.c.obj"
+  "CMakeFiles/OnlineCinemaProjectMy.dir/films.c.obj.d"
+  "OnlineCinemaProjectMy.exe"
+  "OnlineCinemaProjectMy.exe.manifest"
+  "OnlineCinemaProjectMy.pdb"
+  "libOnlineCinemaProjectMy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OnlineCinemaProjectMy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
